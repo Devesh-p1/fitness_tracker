1d55763ab240e7fe36d50eef9c3257546a39e6b4
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-12 17:36:03 -0400

    added put to api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,3 +9,16 @@ router.post("/api/workouts", (req, res) => {
         res.json(err);
       });
   });
+  router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then(over9000db => {
+        res.json(over900db);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
