9b3394d9d45b02ac292a2d9b26a40860c695a18b
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-12 17:24:12 -0400

    added them workouts to models

--- models/model.js
+++ models/model.js
@@ -6,7 +6,38 @@ const Schema = new Schema({
         type: Date,
         default: () => new Date()
       },
-});
+      exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise type"
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise name"
+          },
+          duration: {
+            type: Number,
+            required: "Enter an exercise duration in minutes"
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
+        }
+      ]
+    },
+);
 
 const dataBase = mongoose.model("", Schema);
 
