85f3f922e8949e20e26c58db02c484de645d892a
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-12 17:16:09 -0400

    fixing server.js

---
+++ models/model.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Schema = new Schema({});
+
+const dataBase = mongoose.model("", Schema);
+
+module.exports = dataBase;
