227d27e0bdc529eaed2568e4eef0ce3ec8f3e30f
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-13 18:06:06 -0400

    more server bug fixes

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/over9000db", { 
-  useNewUrlParser: true.valueOf,
-  // useFindandModify: false,
-  useUnifiedTopology: true  
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 app.use(require("./routes/apiRoutes.js"));
