2fb6f1278d432203b0b855ca201239ec21007b17
Devesh <83361356+Devesh-p1@users.noreply.github.com>
2021-07-13 17:35:01 -0400

    added server side stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const password = require("./ignore/ignore.js")
 const PORT = process.env.PORT || 3000;
 
 const User = require("./models/model.js");
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/over9000db", { 
+mongoose.connect("mongodb+srv://devesh:"+password+"@cluster0.h4dgy.mongodb.net/over9000db?retryWrites=true&w=majority", { 
   useNewUrlParser: true.valueOf,
   // useFindandModify: false,
   useUnifiedTopology: true  
@@ -27,3 +27,5 @@ app.use(require("./routes/viewRoutes.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+console.log(password)
